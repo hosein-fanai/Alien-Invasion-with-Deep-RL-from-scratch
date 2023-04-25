6b09b5ace407b7222b1856f6e761eab00d96e937
hosein-fanai <hosein.fanai@gmail.com>
2023-04-25 08:32:32 +0330

    First chapter is done.

---
+++ alien_invasion.py
@@ -0,0 +1,86 @@
+import pygame
+
+import sys
+
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
+
+
+class AlienInvasion():
+
+    def __init__(self):
+        pygame.init()
+
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            # self.settings.screen_dims,
+            (0, 0), 
+            flags=pygame.FULLSCREEN
+        )
+        self.screen_rect = self.screen.get_rect()
+        self.settings.screen_dims = self.screen_rect.width, self.screen_rect.height
+
+        self.clock = pygame.time.Clock()
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_bullets()
+            self._update_screen()            
+            self.clock.tick(60)
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+                sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        self.ship.blitme()
+
+        pygame.display.flip()
+
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullets_allowed:
+            self.bullets.add(Bullet(self))
+
+    def _update_bullets(self):
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom < 0:
+                self.bullets.remove(bullet)
+
+
+if __name__ == "__main__":
+    ai_game = AlienInvasion()
+    ai_game.run_game()
