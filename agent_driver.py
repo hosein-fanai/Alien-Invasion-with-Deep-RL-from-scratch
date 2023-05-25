612700c990b8781eecfc92e40fe1ec145fa9582e
hosein-fanai <hosein.fanai@gmail.com>
2023-05-25 20:05:40 +0330

    Fixed agent driver.

--- agent_driver.py
+++ agent_driver.py
@@ -1,35 +1,41 @@
-import tensorflow as tf
 from tensorflow.keras import models
 
-from utils import make_env
+from utils import make_env, DQNAgent
 
 import sys
 
 import time
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
+
     try:
-        model_path = sys.argv[0]
+        model_path = sys.argv[1]
+
+        if len(sys.argv) == 3:
+            fps = int(sys.argv[2])
+        else:
+            fps = 60
     except:
-        model_path = "models/old/Alien Invasion DQN (100 iters).h5"
+        model_path = "models/Alien Invasion DQN (425%500 iters & 0.1568 epsilon).h5"
+
+    env = make_env(game_resolution=(720, 720))
+    obs = env.reset()
 
     model = models.load_model(model_path)
-
-    env = make_env()
-
-    obs = env.reset()
+    agent = DQNAgent(env, model, None, None, None, None)
+    
     done = False
     rewards = 0
 
     frames = 0
     start = time.time()
     while not done:
-        action = tf.argmax(model(obs[None]), axis=1)
+        action = agent.epsilon_greedy_policy(obs, 0.1568)
         obs, reward, done, info = env.step(action)
         rewards += reward
 
-        env.game.clock.tick(60)
+        env.game.clock.tick(fps)
         frames += 1
     env.close()
 
